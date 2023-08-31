9778a214cb6381c0685403f8eed44b4da90a7cb0
Endo Axelsson <endo.axelsson@elev.ga.ntig.se>
2023-08-31 11:01:13 +0200

    ✨ variabel kod

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 console.log("hello")
+
+
+let firstName;
+Firstname="Endo"
+let lastName = "axelsson"
+
+
+let num = 200
+console.log(typeof(num))
+
+
+var NAME ="Stina"
+console.log(NAME)
+
+
+var skola = "NTI"
+console.log(skola)
+
+
+let i;
+
+for(i = 0;i<10; i++) {
+    let NAME =- "Sture"
+    console.log(NAME)
+
+}
+
+let a = "Jens"
+let b = "jens"
+let c = `Mitt namn är ${a}´
+let d = `Mitt namn är´ + b
+
+
+let e=12
+let f = 17.7
+
+let g =true
+let h =false
+
+console.log(c,e, g)
